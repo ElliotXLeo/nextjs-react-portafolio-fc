000d2c81a39fa73480b84c1fcabc9bb056b1892c
ElliotXLeo <elliotgaramendi@gmail.com>
2021-09-05 16:12:40 -0500

    Uninstall nprogress

--- components/Layaout.js
+++ components/Layaout.js
@@ -1,30 +1,11 @@
 import { useRouter } from 'next/dist/client/router';
-import nProgress from "nProgress";
-import { Fragment, useEffect } from 'react';
+import { Fragment } from 'react';
 import Footer from './Footer';
 import Navbar from './Navbar';
 
 const Layaout = ({ children, profile, seeFooter = true }) => {
 
   const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      console.log(url);
-      nProgress.start();
-    };
-
-    router.events.on('routeChangeStart', handleRouteChange);
-    router.events.on('routeChangeComplete', () => nProgress.done());
-    // router.events.on("routeChangeError", () => nProgress.done());
-
-    return (
-      () => {
-        router.events.off('routeChangeStart', handleRouteChange);
-      }
-    );
-  }, []);
-
 
   return (
     <Fragment>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css/animate.min.css';
 import '../global.css';
-import 'nprogress/nprogress.css';
 
 function MyApp({ Component, pageProps }) {
   return (<Component {...pageProps} />);
